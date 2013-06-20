ec39a5e05e7649b9183b21b6ee0d1378507a8db3
dzucconi <damonzucconi@gmail.com>
2013-06-20 10:57:34 -0400

    Add grunt-shell and include docs generation in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,24 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
     uglify: {
       plugin: {
         files: [{
-          'build/ephemerides.min.js': ['src/ephemerides.astro.js', 'src/ephemerides.calc.js', 'src/ephemerides.calendar.js']
+          "build/ephemerides.min.js": ["src/ephemerides.astro.js", "src/ephemerides.calc.js", "src/ephemerides.calendar.js"]
         }]
+      }
+    },
+
+    shell: {
+      generateDocs: {
+        command: "docco src/ephemerides.*.js"
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-shell");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
 
-  grunt.registerTask('build', 'uglify');
+  grunt.registerTask("build", ["uglify", "shell:generateDocs"]);
 };
