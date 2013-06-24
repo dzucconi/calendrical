d969ec067e376f2b3a3667563ed3012b6cf1b4a9
dzucconi <damonzucconi@gmail.com>
2013-06-24 17:17:06 -0400

    Augment date with helper methods

--- src/calendrical.date.js
+++ src/calendrical.date.js
@@ -1,9 +1,36 @@
 // Extensions to the Date prototype
 ;(function() {
+  "use strict";
+
   Date.prototype.getJulian = function() {
-    return (
-      (this / 86400000) - (this.getTimezoneOffset() / 1440) +
-        Calendrical.calendar.constants.J1970
-    );
+    return Calendrical.calendar.gregorianToJd(
+      this.getFullYear(), (this.getMonth() + 1), this.getDate()) +
+      (Math.floor(
+        this.getSeconds() + 60 * (this.getMinutes() + 60 * this.getHours()) + 0.5
+      ) / 86400.0);
   }
+
+  var methods = [
+    "ModifiedJulianDay",
+    "Hebrew",
+    "Islamic",
+    "Persian",
+    "Persiana",
+    "Mayan",
+    "Bahai",
+    "IndianCivil",
+    "FrenchRevolutionary",
+    "GregorianSerial",
+    "Excel1900",
+    "Excel1904",
+    "UnixTime",
+    "IsoWeek",
+    "IsoDay"
+  ];
+
+  methods.map(function(method) {
+    Date.prototype["get" + method] = function() {
+      return Calendrical.calendar["update" + method](this.getJulian());
+    }
+  });
 }());
