3eed10ffd7b4d867f32b4966a2e09a04d00a5a37
dzucconi <damonzucconi@gmail.com>
2013-06-24 16:44:48 -0400

    Start on Date extensions

--- src/calendrical.date.js
+++ src/calendrical.date.js
@@ -1,2 +1,9 @@
-// Extensions to the Date Prototype
-
+// Extensions to the Date prototype
+;(function() {
+  Date.prototype.getJulian = function() {
+    return (
+      (this / 86400000) - (this.getTimezoneOffset() / 1440) +
+        Calendrical.calendar.constants.J1970
+    );
+  }
+}());
