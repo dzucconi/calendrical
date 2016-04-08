c440ad88aaae844f6a770d631e7ac8655821b413
Carlo Dapor <catull@gmail.com>
2016-04-08 18:20:55 +0200

    More corrections due to 1-off fixes.

--- test/spec/calendar.bahai.spec.js
+++ test/spec/calendar.bahai.spec.js
@@ -4,15 +4,9 @@
 'use strict';
 
 describe ("Bahai calendar spec", function () {
-  var cal, date, julian;
-
-  beforeEach (function () {
-      cal = Calendrical.calendar;
-      date = new Date (2013, 5, 24, 21, 0, 0); // Mon Jun 24 2013 21:00:00 GMT
+  var cal = Calendrical.calendar,
+      date = new Date (2013, 5, 24),
       julian = date.getJulian ();
-
-    cal.updateBahai (julian);
-  });
 
   it ("should convert a date to Bahai calendar", function () {
     expect (date.getBahai()).toEqual ({
@@ -20,17 +14,32 @@ describe ("Bahai calendar spec", function () {
         vahid: 9,
         year: 'Abhá',
         month: 'Rahmat',
-        day: 'Jalál',
-        weekday: 'Kamál',
-        leap: false }
+        day: 'Bahá',
+        weekday: 'Jalál',
+        leap: false,
+        official: true
+      }
     );
   });
 
   it ("should convert a Bahai date to Julian day", function () {
-    expect (cal.bahaiToJd(1, 9, 18, 6, 2)).toEqual (julian + 0.125);
+    expect (cal.bahaiToJd(1, 9, 18, 6, 1)).toEqual (julian);
   });
 
   it ("should convert a Julian day to a Bahai date", function () {
-    expect (cal.jdToBahai(julian)).toEqual ([ 1, 9, 18, 6, 2 ]);
+    expect (cal.jdToBahai(julian)).toEqual ([ 1, 9, 18, 6, 1 ]);
+  });
+
+  it ("should determine whether a Bahai year is leap year", function () {
+    // the Bahai years 1 and 169 are the limits of the old leap rule
+    expect (cal.leapBahai(1)).toBe (true);
+    expect (cal.leapBahai(168)).toBe (false);
+    expect (cal.leapBahai(169)).toBe (true);
+    expect (cal.leapBahai(170)).toBe (false);
+
+    // starting with the Bahai year 172, the new rule is in place
+    expect (cal.leapBahai(173)).toBe (false);
+    expect (cal.leapBahai(174)).toBe (true);
+    expect (cal.leapBahai(220)).toBe (true);
   });
 });
