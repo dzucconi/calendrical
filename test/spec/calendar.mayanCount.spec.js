c2595bbb37a92969e723ba23d0efc84bc0714705
Carlo Dapor <catull@gmail.com>
2016-04-17 22:38:47 +0200

    Cosmetic changes of spaces.

--- test/spec/calendar.mayanCount.spec.js
+++ test/spec/calendar.mayanCount.spec.js
@@ -10,8 +10,8 @@ describe ("Mayan Count calendar spec", function () {
   it ("should convert a Mayan Count to Julian day", function () {
     data2.forEach (function (data) {
         expected = data.rataDie + cal.constants.J0000;
-        date = data.mayanLong;
-        actual = cal.mayanCountToJd (date.baktun, date.katun, date.tun, date.uinal, date.kin);
+        date     = data.mayanLong;
+        actual   = cal.mayanCountToJd (date.baktun, date.katun, date.tun, date.uinal, date.kin);
 
         expect (expected).toEqual (actual);
     });
@@ -19,10 +19,10 @@ describe ("Mayan Count calendar spec", function () {
 
   it ("should convert a Julian day to a Mayan Count", function () {
     data2.forEach (function (data) {
-        julian = data.rataDie + cal.constants.J0000;
-        date = data.mayanLong;
+        julian   = data.rataDie + cal.constants.J0000;
+        date     = data.mayanLong;
         expected = [ date.baktun, date.katun, date.tun, date.uinal, date.kin ];
-        actual = cal.jdToMayanCount (julian);
+        actual   = cal.jdToMayanCount (julian);
 
         expect (expected).toEqual (actual);
     });
