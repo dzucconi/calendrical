80c0712fde94e235bde4da4b04163fdd5476e888
Carlo Dapor <catull@gmail.com>
2016-04-17 22:17:20 +0200

    Added Mayan Haab calendar tests.

---
+++ test/spec/calendar.mayanHaab.spec.js
@@ -0,0 +1,20 @@
+/* global Calendrical data2 describe it expect:true*/
+/* eslint no-undef: "error"*/
+
+'use strict';
+
+describe ("Mayan Haab calendar spec", function () {
+  var cal = Calendrical.calendar,
+      date, julian, expected, actual;
+
+  it ("should convert a Julian day to a Mayan Haab", function () {
+    data2.forEach (function (data) {
+        julian   = data.rataDie + cal.constants.J0000;
+        date     = data.mayanHaab;
+        expected = [ date.month, date.day ];
+        actual   = cal.jdToMayanHaab (julian);
+
+        expect (expected).toEqual (actual);
+    });
+  });
+});
