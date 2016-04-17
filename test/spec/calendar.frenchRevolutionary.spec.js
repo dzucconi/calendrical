d9f5f3a35ab303b7515ea7029c170498f1acbdc1
Carlo Dapor <catull@gmail.com>
2016-04-17 21:08:57 +0200

    Added French revolutionary calendar tests.

---
+++ test/spec/calendar.frenchRevolutionary.spec.js
@@ -0,0 +1,36 @@
+/* global Calendrical data3 describe it expect:true*/
+/* eslint no-undef: "error"*/
+
+'use strict';
+
+describe ("French revolutionary calendar spec", function () {
+  var astro, cal, date, expected, actual, decade, jour;
+
+  astro = Calendrical.astro;
+  cal = Calendrical.calendar;
+
+  it ("should convert a French revolutionary date to Julian day", function () {
+    data3.forEach (function (data) {
+        date     = data.french;
+        expected = data.rataDie + cal.constants.J0000;
+        jour     = date.day;
+        decade   = Math.floor ((jour - 1) / 10) + 1;
+        jour     = astro.amod (jour, 10);
+        actual   = cal.frenchRevolutionaryToJd (date.year, date.month, decade, jour);
+        expect (expected).toEqual (actual);
+    });
+  });
+
+  it ("should convert a Julian day to a French revolutionary date", function () {
+    data3.forEach (function (data) {
+        date     = data.french;
+        jour     = date.day;
+        decade   = Math.floor ((jour - 1) / 10) + 1;
+        jour     = astro.amod (jour, 10);
+        expected = [ date.year, date.month, decade, jour ];
+        actual   = cal.jdToFrenchRevolutionary (data.rataDie + cal.constants.J0000);
+
+        expect (expected).toEqual (actual);
+    });
+  });
+});
