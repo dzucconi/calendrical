1b5618fb3003c8636dc96a2080161c9b61ea2ebe
Carlo Dapor <catull@gmail.com>
2016-05-03 18:47:13 +0200

    Implemented persian arithmetic calendar.

--- test/spec/calendar.persianArithmetic.spec.js
+++ test/spec/calendar.persianArithmetic.spec.js
@@ -3,65 +3,47 @@
 
 'use strict';
 
-describe ("Persian astronomical calendar spec", function () {
+describe ("Persian arithmetic calendar spec", function () {
   var cal, date, expected, actual;
 
   cal = Calendrical.calendar;
 
-  it ("should convert a Persian astronomical date to Julian day", function () {
-      expected = 171307;
-      actual = cal.persianToJd (-153, 10, 18);
-      expect (expected).toEqual (actual);
-
-      // expected = 210155;
-      // actual = cal.persianToJd (-46, 2, 30);
-      // expect (expected).toEqual (actual);
-
-      // expected = 400085;
-      // actual = cal.persianToJd (475, 3, 3);
-      // expect (expected).toEqual (actual);
+  it ("should convert a Persian arithmetic date to Julian day", function () {
     data3.forEach (function (data) {
-        //if (data.rataDie !== 171307 && data.rataDie !== 210155 && data.rataDie !== 400085) {
-            date     = data.persianAstro;
-            // expected = data.rataDie + cal.constants.J0000;
-            expected = data.rataDie;
-            actual   = cal.persianToJd (date.year, date.month, date.day);
-            expect (expected).toEqual (actual);
-        //}
+        date     = data.persianArith;
+        expected = data.rataDie + cal.constants.J0000;
+        // expected = data.rataDie;
+        actual   = cal.persianArithmeticToJd (date.year, date.month, date.day);
+        expect (expected).toEqual (actual);
     });
   });
 
-  it ("should convert a Julian day to a Persian astronomical date", function () {
-      expected = [ -153, 10, 18 ];
-      actual = cal.jdToPersian (171307);
-      expect (expected).toEqual (actual);
-
-      // expected = [ -46, 2, 30 ];
-      // actual = cal.jdToPersian (210155);
-      // expect (expected).toEqual (actual);
-
-      // expected = [ 475, 3, 3 ];
-      // actual = cal.jdToPersian (400085);
-      // expect (expected).toEqual (actual);
-
+  it ("should convert a Julian day to a Persian arithmetic year", function () {
     data3.forEach (function (data) {
-        //if (data.rataDie !== 171307 && data.rataDie !== 210155 && data.rataDie !== 400085) {
-            date     = data.persianAstro;
-            expected = [ date.year, date.month, date.day ];
-            // actual   = cal.jdToPersian (data.rataDie + cal.constants.J0000);
-            actual   = cal.jdToPersian (data.rataDie);
-            expect (expected).toEqual (actual);
-        //}
+        date     = data.persianArith;
+        expected = date.year;
+        actual   = cal.jdToPersianArithmeticYear (data.rataDie + cal.constants.J0000);
+        expect (expected).toEqual (actual);
     });
   });
 
-  it ("should determine whether a Persian astronomical year is leap year", function () {
-      [ 4 ].forEach (function (year) {
-          expect (cal.leapPersian (year)).toBe (true);
+  it ("should convert a Julian day to a Persian arithmetic date", function () {
+    data3.forEach (function (data) {
+        date     = data.persianArith;
+        expected = [ date.year, date.month, date.day ];
+        actual   = cal.jdToPersianArithmetic (data.rataDie + cal.constants.J0000);
+        expect (expected).toEqual (actual);
+    });
+  });
+
+  it ("should determine whether a Persian arithmetic year is leap year", function () {
+      [ 4, 124, 165, 206, 739, 780, 821, 1313, 1354, 1395 ].forEach (function (year) {
+          expect (cal.leapPersianArithmetic (year)).toBe (true);
       });
 
-      [ 1, 2, 3 ].forEach (function (year) {
-          expect (cal.leapPersian (year)).toBe (false);
+      [ 1, 48, 142, 189, 236, 283, 377, 424, 471, 518, 612, 659, 753, 800, 847,
+        894, 988, 1035, 1082, 1129, 1223, 1270, 1364 ].forEach (function (year) {
+          expect (cal.leapPersianArithmetic (year)).toBe (false);
       });
   });
 });
