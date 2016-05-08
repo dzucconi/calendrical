da8a9410ee1f36d170af676908ce66d0ff945e53
Carlo Dapor <catull@gmail.com>
2016-05-08 21:02:59 +0200

    Added karma as dependency, and 2 npm script targets: karma-dev for development, karma-batch for one-off runs.  Unfortunately, phantomjs-prebuilt does not accept back-quote string interpolation ...

---
+++ karma.headless.conf.js
@@ -0,0 +1,28 @@
+// Karma configuration
+// Generated on Sun May 08 2016 20:26:26 GMT+0200 (CEST)
+
+module.exports = function (config) {
+  config.set ({
+
+    basePath: '',
+    frameworks: [ 'jasmine' ],
+    files: [
+      'src/*.js',
+      'test/spec/data/data?.js',
+      'test/spec/*.spec.js'
+    ],
+    exclude: [
+      'bower_components/**/*',
+      'node_modules/**/*'
+    ],
+    preprocessors: { },
+    reporters: [ 'progress' ],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: [ 'PhantomJS' ],
+    singleRun: true,
+    concurrency: 1
+  });
+};
